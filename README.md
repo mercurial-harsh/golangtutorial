8fea982a9e158262d709216aeab929566bea719f
mercurial_harsh <harshguptaucme@gmail.com>
2021-03-15 21:38:10 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+##
+arithmatic operator
+ +,-,*,/,--,++,%
+##
+
+comparison operator
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name1 string = "harsh"
+	fmt.Println(name1)
+
+	name2:="gupta"
+	
+
+	const num1 int = 12
+
+	fmt.Println(name2,num1)
+
+}
